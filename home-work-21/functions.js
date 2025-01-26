0278ab54641e7e8b3959c8fcdfe88db67de77843
Alex Kirichenko <tensor1989@gmail.com>
2025-01-26 20:04:20 +0200

    hw21

--- home-work-21/functions.js
+++ home-work-21/functions.js
@@ -8,8 +8,8 @@ export function generateKey(length, characters) {
 }
 
 export function average(mixed) {
-    const numbersArray = mixed.filter(elem => !isNaN(Number(elem)))
-        .map(el => Number(el));
+    const numbersArray = mixed.map(el => Number(el))
+        .filter(el => !isNaN(el));
     const sum = numbersArray.reduce((a, b) => a + b, 0);
     console.log(numbersArray);
     return sum / numbersArray.length;
@@ -18,11 +18,7 @@ export function average(mixed) {
 export function showDeepArray(deep){
     if(Array.isArray(deep)){
         deep.forEach(elem => {
-            if(Array.isArray(elem)) {
-                showDeepArray(elem)
-            }else{
-                console.log(elem);
-            }
+            showDeepArray(elem);
         });
     }else {
         console.log(deep);
