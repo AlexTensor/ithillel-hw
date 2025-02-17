94b2f508e45e76cbc19317bab0a7444022d29120
Alex Kirichenko <tensor1989@gmail.com>
2025-02-17 16:07:09 +0200

    hw27 inprogress

--- home-work-27/main.js
+++ home-work-27/main.js
@@ -1,10 +1,13 @@
 
+
+let currentSlide = 0;
+const imagesCount = document.querySelectorAll('.slider__container > img').length;
+let interval = null;
 setSliderParams();
 createSliderBtns();
-let currentSlider = 0;
 
 const arrowBtnElements = document.querySelectorAll('.slider__navigation > .btn');
-[...arrowBtnElements].forEach((btn) => btn.addEventListener('click', arrowBtnEvent));
+[...arrowBtnElements].forEach((btn) => btn.addEventListener('click', moveSliderEvent));
 
 const playBtnElement = document.querySelector('.play-pause-btn');
 playBtnElement.addEventListener('click', playPauseEvent);
@@ -12,18 +15,29 @@ playBtnElement.addEventListener('click', playPauseEvent);
 const slideBySlideBtnElements = document.querySelectorAll('.slideBySlide-btn > div');
 [...slideBySlideBtnElements].forEach((btn) => btn.addEventListener('click', slideBySlideEvent));
 
+const sliderElement = document.getElementById('slider');
+sliderElement.addEventListener('touchend', moveSliderEvent);
+
 function slideBySlideEvent(event) {
     const btnElement = event.target;
     const slideBySlideBtnElements = document.querySelectorAll('.slideBySlide-btn > div');
     if(!btnElement.classList.contains('active')) {
-        [...slideBySlideBtnElements].forEach((btnElement, index) => btnElement.classList.remove('active'));
+        [...slideBySlideBtnElements].forEach((btnElement) => btnElement.classList.remove('active'));
         btnElement.classList.add('active');
         [...slideBySlideBtnElements].forEach((btnElement, index) =>{
             if(btnElement.classList.contains('active')) {
-                currentSlider = index;
+                currentSlide = index;
             }
         });
+        moveSlide(currentSlide);
     }
+}
+
+function moveSlide(currentSlide){
+    const sliderElement = document.getElementById('slider');
+    const sliderWidth = sliderElement.dataset.width;
+    const sliderContent = document.querySelector('.slider__container');
+    sliderContent.style.transform = `translateX(-${sliderWidth * currentSlide}px)`;
 }
 
 function playPauseEvent(event) {
@@ -32,21 +46,43 @@ function playPauseEvent(event) {
         btnElement.innerHTML = '&#10073;&#10073;';
         btnElement.classList.remove('play');
         btnElement.classList.add('pause');
+        const intervalValue = document.getElementById('slider').dataset.interval;
+        interval = setInterval(() => {
+            moveSliderEvent(event, 'play');
+        }, intervalValue * 1000);
     }else{
         btnElement.innerHTML = '&#9658;';
         btnElement.classList.remove('pause');
         btnElement.classList.add('play');
+        clearInterval(interval);
     }
 }
 
-function arrowBtnEvent(event) {
-    if(event.target.classList.contains('right')) {
-        console.log(2);
-    }else{
-        console.log(1);
+function moveSliderEvent(event, action) {
+
+    console.log(event);
+
+    if(event.target.classList.contains('right') || action === 'play') {
+        currentSlide += 1;
+    }else if(event.target.classList.contains('left')){
+        currentSlide -= 1;
     }
+
+    if(currentSlide >= imagesCount){
+        currentSlide = 0;
+    }else if(currentSlide < 0){
+        currentSlide = imagesCount - 1;
+    }
+
+    moveSlide(currentSlide);
+    markDots();
 }
 
+function markDots(){
+    const slideBySlideBtnElements = document.querySelectorAll('.slideBySlide-btn > div');
+    [...slideBySlideBtnElements].forEach((btnElement) => btnElement.classList.remove('active'));
+    [...slideBySlideBtnElements][currentSlide].classList.add('active');
+}
 
 function setSliderParams(){
     const sliderElement = document.getElementById('slider');
@@ -54,12 +90,10 @@ function setSliderParams(){
     const sliderHeight = sliderElement.dataset.height;
     sliderElement.style.width = `${sliderWidth}px`;
     sliderElement.style.height = `${sliderHeight}px`;
-    // const sliderContainer = document.getElementsByClassName('slider__container')[0];
-    // sliderContainer.style.height = `${sliderHeight}px`;
 }
 
 function createSliderBtns(){
-    const imagesCount = document.querySelectorAll('.slider__container > img').length;
+
     let resultHTML = '';
     for(let i = 0; i < imagesCount; i++) {
         if(!i){
